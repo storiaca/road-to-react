7d6d69694719241ee6db8fe2d91bf2fc7f8abf8b
storiaca <storiaca@gmail.com>
2020-04-10 15:17:08 +0200

    Lists in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from "react";
 import "./App.css";
 
-function getTitle(title) {
-  return title;
-}
-
-// const welcome = {
-//   greeting: "Hey",
-//   title: "React",
-// };
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello {getTitle("React")}</h1>
-      {/* <h1>
-        {welcome.greeting} {welcome.title}
-      </h1> */}
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
       <input type="text" id="search" />
+      <hr />
+      {list.map(function (item) {
+        return (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
