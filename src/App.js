c9e2ff4f804b2454d9739ebaea0c56bf7d455e71
storiaca <storiaca@gmail.com>
2020-04-21 10:31:33 +0200

    Third-party libraries in React (axios)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef, useReducer } from "react";
+import axios from "axios";
 import "./App.css";
 
 const useSemiPersistentState = (key, initialState) => {
@@ -56,16 +57,17 @@ const App = () => {
 
   const handleFetchStories = React.useCallback(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
-    fetch(url)
-      .then((response) => response.json())
+    axios
+      .get(url)
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
   }, [url]);
+
   useEffect(() => {
     handleFetchStories();
   }, [handleFetchStories]);
