fb07ec0baa4237c1938f003525af6da82fc1af3a
Aleksandar Ristic <storiaca@gmail.com>
2020-05-08 14:33:22 +0200

    Typscript in react finished

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,65 @@ import axios from "axios";
 import { ReactComponent as Check } from "./check.svg";
 import "./App.css";
 
-const useSemiPersistentState = (key: string, initialState: string) => {
-  const isMounted = useRef(false);
-  const [value, setValue] = useState(localStorage.getItem(key) || initialState);
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
+
+const useSemiPersistentState = (
+  key: string,
+  initialState: string
+): [string, (newValue: string) => void] => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
 
   React.useEffect(() => {
-    if (!isMounted.current) {
-      isMounted.current = true;
-    } else {
-      localStorage.setItem(key, value);
-    }
     localStorage.setItem(key, value);
   }, [value, key]);
 
   return [value, setValue];
 };
 
-const storiesReducer = (state, action) => {
+type Story = {
+  objectID: string;
+  url: string;
+  title: string;
+  author: string;
+  num_comments: number;
+  points: number;
+};
+
+type Stories = Array<Story>;
+
+type StoriesState = {
+  data: Stories;
+  isLoading: boolean;
+  isError: boolean;
+};
+
+interface StoriesFetchInitAction {
+  type: "STORIES_FETCH_INIT";
+}
+
+interface StoriesFetchSuccessAction {
+  type: "STORIES_FETCH_SUCCESS";
+  payload: Stories;
+}
+
+interface StoriesFetchFailureAction {
+  type: "STORIES_FETCH_FAILURE";
+}
+
+interface StoriesRemoveAction {
+  type: "REMOVE_STORY";
+  payload: Story;
+}
+
+type StoriesAction =
+  | StoriesFetchInitAction
+  | StoriesFetchSuccessAction
+  | StoriesFetchFailureAction
+  | StoriesRemoveAction;
+
+const storiesReducer = (state: StoriesState, action: StoriesAction) => {
   switch (action.type) {
     case "STORIES_FETCH_INIT":
       return {
@@ -58,15 +100,12 @@ const storiesReducer = (state, action) => {
   }
 };
 
-const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
-
-const getSumComments = (stories) => {
-  return stories.data.reduce((result, value) => result + value.num_comments, 0);
-};
 const App = () => {
-  const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "react");
-  const [url, setUrl] = useState(`${API_ENDPOINT}${searchTerm}`);
-  const [stories, dispatchStories] = useReducer(storiesReducer, {
+  const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
+
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
+
+  const [stories, dispatchStories] = React.useReducer(storiesReducer, {
     data: [],
     isLoading: false,
     isError: false,
@@ -74,6 +113,7 @@ const App = () => {
 
   const handleFetchStories = React.useCallback(async () => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
+
     try {
       const result = await axios.get(url);
 
@@ -86,49 +126,84 @@ const App = () => {
     }
   }, [url]);
 
-  useEffect(() => {
+  React.useEffect(() => {
     handleFetchStories();
   }, [handleFetchStories]);
 
-  useEffect(() => {
-    localStorage.setItem("search", searchTerm);
-  }, [searchTerm]);
-
-  const handleSearchInput = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSearchSubmit = (event) => {
-    event.preventDefault();
-    setUrl(`${API_ENDPOINT}${searchTerm}`);
-  };
-
-  const handleRemoveStory = useCallback((item) => {
+  const handleRemoveStory = (item: Story) => {
     dispatchStories({
       type: "REMOVE_STORY",
       payload: item,
     });
-  }, []);
+  };
 
-  const sumComments = React.useMemo(() => getSumComments(stories), [stories]);
+  const handleSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+    event.preventDefault();
+  };
+
   return (
-    <div className="container">
-      <h1 className="headline-primary">
-        My Hacker Stories with {sumComments} comments.
-      </h1>
+    <div>
+      <h1>My Hacker Stories</h1>
+
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
       />
+
+      <hr />
+
       {stories.isError && <p>Something went wrong ...</p>}
+
       {stories.isLoading ? (
-        <p>Loading...</p>
+        <p>Loading ...</p>
       ) : (
         <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
     </div>
   );
+};
+
+type SearchFormProps = {
+  searchTerm: string;
+  onSearchInput: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onSearchSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+};
+
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit,
+}: SearchFormProps) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+
+    <button type="submit" disabled={!searchTerm}>
+      Submit
+    </button>
+  </form>
+);
+
+type InputWithLabelProps = {
+  id: string;
+  value: string;
+  type?: string;
+  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  isFocused?: boolean;
+  children: React.ReactNode;
 };
 
 const InputWithLabel = ({
@@ -138,79 +213,62 @@ const InputWithLabel = ({
   onInputChange,
   isFocused,
   children,
-}) => {
-  const inputRef = useRef();
+}: InputWithLabelProps) => {
+  const inputRef = React.useRef<HTMLInputElement>(null!);
 
-  useEffect(() => {
+  React.useEffect(() => {
     if (isFocused && inputRef.current) {
       inputRef.current.focus();
     }
   }, [isFocused]);
+
   return (
     <>
-      <label htmlFor={id} className="label">
-        {children}
-      </label>
+      <label htmlFor={id}>{children}</label>
       &nbsp;
       <input
         ref={inputRef}
-        type={type}
         id={id}
+        type={type}
         value={value}
         onChange={onInputChange}
-        className="input"
       />
     </>
   );
 };
 
-const List = React.memo(({ list, onRemoveItem }) =>
-  list.map((item) => (
-    <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
-  ))
-);
-
-const Item = ({ item, onRemoveItem }) => {
-  //const handleRemoveItem = () => onRemoveItem(item);
-  return (
-    <div className="item">
-      <span style={{ width: "40%" }}>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span style={{ width: "30%" }}>{item.author}</span>
-      <span style={{ width: "10%" }}>{item.num_comments}</span>
-      <span style={{ width: "10%" }}>{item.points}</span>
-      <span style={{ width: "10%" }}>
-        <button
-          type="button"
-          onClick={() => onRemoveItem(item)}
-          className="button button_small"
-        >
-          <Check height="18px" width="18px" />
-        </button>
-      </span>
-    </div>
-  );
+type ListProps = {
+  list: Stories;
+  onRemoveItem: (item: Story) => void;
 };
 
-const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
-  <form onSubmit={onSearchSubmit} className="search-form">
-    <InputWithLabel
-      id="search"
-      label="Search"
-      value={searchTerm}
-      isFocused
-      onInputChange={onSearchInput}
-    >
-      <strong>Search:</strong>
-    </InputWithLabel>
-    <button
-      type="submit"
-      disabled={!searchTerm}
-      className="button button_large"
-    >
-      Submit
-    </button>
-  </form>
+const List = ({ list, onRemoveItem }: ListProps) => (
+  <>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+    ))}
+  </>
 );
+
+type ItemProps = {
+  item: Story;
+  onRemoveItem: (item: Story) => void;
+};
+
+const Item = ({ item, onRemoveItem }: ItemProps) => (
+  <div>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
+  </div>
+);
+
 export default App;
