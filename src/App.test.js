76e857c41e6e8e85bc497c27cc7733fb0f6ca753
Aleksandar Ristic <storiaca@gmail.com>
2020-05-10 10:54:05 +0200

    Test for List and SearchForm

--- src/App.test.js
+++ src/App.test.js
@@ -19,8 +19,17 @@ describe("Item", () => {
     points: 4,
     objectID: 0,
   };
+
+  const handleRemoveItem = jest.fn();
+
+  let component;
+
+  beforeEach(() => {
+    component = renderer.create(
+      <Item item={item} onRemoveItem={handleRemoveItem} />
+    );
+  });
   it("renders all properties", () => {
-    const component = renderer.create(<Item item={item} />);
     expect(component.root.findByType("a").props.href).toEqual(
       "https://reactjs.org/"
     );
@@ -31,17 +40,58 @@ describe("Item", () => {
   });
 
   it("calls onRemoveItem on button click", () => {
-    const handleRemoveItem = jest.fn();
-
-    const component = renderer.create(
-      <Item item={item} onRemoveItem={handleRemoveItem} />
-    );
     component.root.findByType("button").props.onClick();
 
     expect(handleRemoveItem).toHaveBeenCalledTimes(1);
     expect(handleRemoveItem).toHaveBeenCalledWith(item);
 
     expect(component.root.findAllByType(Item).length).toEqual(1);
+  });
+});
+
+describe("List", () => {
+  const list = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  it("renders two items", () => {
+    const component = renderer.create(<List list={list} />);
+
+    expect(component.root.findAllByType(Item).length).toEqual(2);
+  });
+});
+
+describe("SearchForm", () => {
+  const searchFormProps = {
+    searchTerm: "React",
+    onSearchInput: jest.fn(),
+    onSearchSubmit: jest.fn(),
+  };
+
+  let component;
+
+  beforeEach(() => {
+    component = renderer.create(<SearchForm {...searchFormProps} />);
+  });
+
+  it("renders the input field with its value", () => {
+    const value = component.root.findByType("input").props.value;
+    expect(value).toEqual("React");
   });
 });
 
