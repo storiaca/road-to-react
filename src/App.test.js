3d83ea38c280be31a09e7989c8220d69937e91ae
Aleksandar Ristic <storiaca@gmail.com>
2020-05-11 12:50:19 +0200

    Test App

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,70 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import renderer from "react-test-renderer";
+import axios from "axios";
 
 import App, { Item, List, SearchForm, InputWithLabel } from "./App";
+
+jest.mock("axios");
 
 // test('renders learn react link', () => {
 //   const { getByText } = render(<App />);
 //   const linkElement = getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
+
+describe("App", () => {
+  it("succeeds fetching data with a list", async () => {
+    const list = [
+      {
+        title: "React",
+        url: "https://reactjs.org/",
+        author: "Jordan Walke",
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+      },
+      {
+        title: "Redux",
+        url: "https://redux.js.org/",
+        author: "Dan Abramov, Andrew Clark",
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+      },
+    ];
+
+    const promise = Promise.resolve({
+      data: {
+        hits: list,
+      },
+    });
+
+    axios.get.mockImplementationOnce(() => promise);
+
+    let component;
+    await renderer.act(async () => {
+      component = renderer.create(<App />);
+    });
+    expect(component.root.findByType(List).props.list).toEqual(list);
+  });
+
+  it("fails fetching data with a list", async () => {
+    const promise = Promise.reject();
+
+    axios.get.mockImplementationOnce(() => promise);
+
+    let component;
+
+    await renderer.act(async () => {
+      component = renderer.create(<App />);
+    });
+
+    expect(component.root.findByType("p").props.children).toEqual(
+      "Something went wrong ..."
+    );
+  });
+});
 
 describe("Item", () => {
   const item = {
@@ -92,6 +148,30 @@ describe("SearchForm", () => {
   it("renders the input field with its value", () => {
     const value = component.root.findByType("input").props.value;
     expect(value).toEqual("React");
+  });
+
+  it("changes the input field", () => {
+    const pseudoEvent = { target: "Redux" };
+
+    component.root.findByType("input").props.onChange(pseudoEvent);
+
+    expect(searchFormProps.onSearchInput).toHaveBeenCalledTimes(1);
+    expect(searchFormProps.onSearchInput).toHaveBeenCalledWith(pseudoEvent);
+  });
+
+  it("submits the form", () => {
+    const pseudoEvent = {};
+
+    component.root.findByType("form").props.onSubmit(pseudoEvent);
+
+    expect(searchFormProps.onSearchSubmit).toHaveBeenCalledTimes(1);
+    expect(searchFormProps.onSearchSubmit).toHaveBeenCalledWith(pseudoEvent);
+  });
+
+  it("disables the button and prevents submit", () => {
+    component.update(<SearchForm {...searchFormProps} searchTerm="" />);
+
+    expect(component.root.findByType("button").props.disabled).toBeTruthy();
   });
 });
 
