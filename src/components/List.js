31c0ea780e24b76623ca5698e591166c25589319
Aleksandar Ristic <storiaca@gmail.com>
2020-05-30 13:36:45 +0200

    Sorting

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { sortBy } from "lodash";
 
 import Item from "./Item";
 
-const List = ({ list, onRemoveItem }) =>
-  list.map((item) => (
-    <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
-  ));
+const SORTS = {
+  NONE: (list) => list,
+  TITLE: (list) => sortBy(list, "title"),
+  AUTHOR: (list) => sortBy(list, "author"),
+  COMMENT: (list) => sortBy(list, "num_comments").reverse(),
+  POINT: (list) => sortBy(list, "points").reverse(),
+};
+const List = ({ list, onRemoveItem }) => {
+  const [sort, setSort] = useState("NONE");
+
+  const handleSort = (sortKey) => {
+    setSort(sortKey);
+  };
+
+  const sortFunctions = SORTS[sort];
+  const sortedList = sortFunctions(list);
+
+  return (
+    <div>
+      <div style={{ display: "flex", marginBottom: "20px" }}>
+        <span style={{ width: "40%" }}>
+          <button type="button" onClick={() => handleSort("TITLE")}>
+            Title
+          </button>
+        </span>
+        <span style={{ width: "30%" }}>
+          <button type="button" onClick={() => handleSort("AUTHOR")}>
+            Author
+          </button>
+        </span>
+        <span style={{ width: "10%" }}>
+          <button type="button" onClick={() => handleSort("COMMENT")}>
+            Comments
+          </button>
+        </span>
+        <span style={{ width: "10%" }}>
+          <button type="button" onClick={() => handleSort("POINT")}>
+            Points
+          </button>
+        </span>
+        <span style={{ width: "10%" }}>Actions</span>
+      </div>
+      {sortedList.map((item) => (
+        <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+      ))}
+    </div>
+  );
+};
 
 export default List;
